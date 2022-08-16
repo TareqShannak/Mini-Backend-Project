5cf452b7ab98916ea2d9df241bf0ada5c286fd1e
tareqshannk <1181404@student.birzeit.edu>
2022-08-16 09:46:53 +0300

    little update on authorities

--- Project-ECS/src/main/java/com/example/demo/UserController.java
+++ Project-ECS/src/main/java/com/example/demo/UserController.java
@@ -28,9 +28,7 @@ public class UserController {
 	@Autowired
 	private ResourceService resourceService;
 
-
 //	For TESTING:
-	
 //	private static final List<Resource> Resources = Arrays.asList(new Resource(1L, "oQattoush@gmail.com", new Date()),
 //			new Resource(2L, "mHafez@gmail.com", new Date()), new Resource(3L, "wSayara@gmail.com", new Date()));
 
@@ -40,7 +38,7 @@ public class UserController {
 	}
 	
 	@GetMapping("/resources")
-	@PreAuthorize("hasAuthority('resource:read')")
+	@PreAuthorize("hasAuthority('resource:write')")
 	public List<Resource> getAllResources() {
 		 return resourceService.allResources();
 	}
@@ -49,20 +47,15 @@ public class UserController {
 	@PreAuthorize("hasAuthority('resource:read')")
 	public Resource getResource(@PathVariable("resourceId") Integer resourceId) {
 		return resourceService.getResourceById(resourceId);
-//		return Resources.stream().filter(resource -> resourceId.equals(resource.getId().intValue())).findFirst()
-//				.orElseThrow(() -> new IllegalStateException("Resource " + resourceId + " does not exists"));
 	}
-
-	
-	//TODO : Generic The Search!
 	
 	@GetMapping("/my_resources")
 	@PreAuthorize("hasAuthority('resource:read')")
 	public Set<Resource> getMyResources() {
-		
-		System.out.println(JwtTokenVerifier.username);
 		return userService.findUserByEmail(JwtTokenVerifier.username).getResources();
 	}
+	
+	//@PostMapping("")
 	
 
 }
